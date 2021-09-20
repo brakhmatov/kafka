6d9da5743eade10bd19b7a8d91595ad81f03024b
Bakhtier Rakhmatov <rakhmatov@gmail.com>
2021-09-20 08:44:12 -0400

    Initial check in of config server

---
+++ parallel-config/src/test/java/com/parallel/config/ParallelConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parallel.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParallelConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
